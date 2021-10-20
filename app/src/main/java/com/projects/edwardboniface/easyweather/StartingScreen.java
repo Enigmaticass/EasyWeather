6e0a8972fd093c85f62e2c6f7f9783e67927d94f
BuildTools <unconfigured@null.spigotmc.org>
2021-10-20 11:49:58 +1000

    Version 1.0

---
+++ app/src/main/java/com/projects/edwardboniface/easyweather/StartingScreen.java
@@ -0,0 +1,14 @@
+package com.projects.edwardboniface.easyweather;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class StartingScreen extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.starting_screen);
+    }
+}
