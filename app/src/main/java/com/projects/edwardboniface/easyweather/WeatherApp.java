6f6816083e1db8b9b8129dfb42169eda718fe7ae
BuildTools <unconfigured@null.spigotmc.org>
2021-11-10 14:57:51 +1000

    Application -> 2.0 (Final)

--- app/src/main/java/com/projects/edwardboniface/easyweather/WeatherApp.java
+++ app/src/main/java/com/projects/edwardboniface/easyweather/WeatherApp.java
@@ -1,5 +1,6 @@
 package com.projects.edwardboniface.easyweather;
 
+import android.graphics.Color;
 import android.os.Bundle;
 import android.util.Log;
 import android.view.View;
@@ -36,6 +37,8 @@ public class WeatherApp extends AppCompatActivity {
         enter_city = findViewById(R.id.enter_city);
         enter_country = findViewById(R.id.enter_country);
         tvResult = findViewById(R.id.tvResult);
+        enter_city.setTextColor(Color.rgb(255,255,255));
+        enter_country.setTextColor(Color.rgb(255,255,255));
 
     }
 
@@ -57,9 +60,51 @@ public class WeatherApp extends AppCompatActivity {
                     //Log.d("response", response);
                     String output = "";
                     try {
+                        //converting JSON response from website into object
                         JSONObject jsonResponse = new JSONObject(response);
+                        //Call the Weather Method from JSON file
                         JSONArray jsonArray = jsonResponse.getJSONArray("weather");
+                        //Get the data from the weather JSON
                         JSONObject jsonObjectWeather = jsonArray.getJSONObject(0);
+                        //Get the string "description" from JSON
+                        String description = jsonObjectWeather.getString("description");
+                        //Go into the Main part of JSON file
+                        JSONObject jsonObjectMain = jsonResponse.getJSONObject("main");
+                        //Find temperature in JSON and take away 273.15 to get degrees celcius
+                        double temp = jsonObjectMain.getDouble("temp") - 273.15;
+                        //Find what the temperature feels like in JSON and take away 273.15 to get degrees celcius
+                        double feelsLike = jsonObjectMain.getDouble("feels_like") - 273.15;
+                        //Find the air pressure from the JSON file
+                        float pressure = jsonObjectMain.getInt("pressure");
+                        //find the humidity from the JSON file
+                        int humidity = jsonObjectMain.getInt("humidity");
+                        //Go into the wind section fo the JSON file
+                        JSONObject jsonObjectWind = jsonResponse.getJSONObject("wind");
+                        //Get the Windspeed from json file
+                        String wind = jsonObjectWind.getString("speed");
+                        //Go into the clouds section of JSON file
+                        JSONObject jsonObjectClouds = jsonResponse.getJSONObject("clouds");
+                        //Get the cloudiness percentage from the JSON file
+                        String clouds = jsonObjectClouds.getString("all");
+                        //Go into the sys seection of the JSON file
+                        JSONObject jsonObjectSys = jsonResponse.getJSONObject("sys");
+                        //Get the country name from the JSON file
+                        String countryName = jsonObjectSys.getString("country");
+                        //Get the city name from the JSON file
+                        String cityName = jsonResponse.getString("name");
+                        //Set the text colour to
+                        tvResult.setTextColor(Color.rgb(255,255,255));
+                        //Set the output
+                        output += "Current weather of " + cityName + " (" + countryName + ")"
+                            + "\n Temp: " + df.format(temp) + " °C"
+                            + "\n Feels Like: " + df.format(feelsLike) + " °C"
+                            + "\n Humidity: " + humidity + "%"
+                            + "\n Description: " + description
+                            + "\n Wind Speed: " + wind + "m/s"
+                            + "\n Cloudiness: " + clouds + "%"
+                            + "\n Pressure: " + pressure + "hPa";
+                        //Assign the output to the text view result
+                        tvResult.setText(output);
                     } catch (JSONException e) {
                         e.printStackTrace();
                     }
